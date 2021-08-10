5aef032614583c7cb8a04f0425db863197dbb579
JulianBarbossa9 <julianbarbossa@hotmail.com>
2021-08-10 15:17:30 -0500

    creación de cubos V1

--- src/script.js
+++ src/script.js
@@ -28,8 +28,22 @@ convertObject.createSphere = () => {
         }
     );
 }
-gui.add(convertObject, 'createSphere'); // Tiene que ser si o si como se llama la función
 
+convertObject.createBox= () => {
+    // Aca le doy diferentes posiciónes y diferente radio a las esferas que estoy creando 
+    createBox(
+        Math.random() * 0.5, // Width
+        Math.random() * 0.5, // Height
+        Math.random() * 0.5, // Depth
+        {
+            x:(Math.random() -0.5) * 3, 
+            y:3, 
+            z:(Math.random() -0.5) * 3
+        }
+    );
+}
+gui.add(convertObject, 'createSphere'); // Tiene que ser si o si como se llama la función
+gui.add(convertObject, 'createBox');
 /**
  * Base
  */
@@ -217,18 +231,20 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 // Creo un array que actualiza los objetos creados Mesh y Body 
 const objectsToUpdate = [];
+//Optimizar
+const sphereGeometry = new THREE.SphereBufferGeometry(1,20,20); 
+const sphereMaterial = new THREE.MeshStandardMaterial({
+    metalness:0.3,
+    roughness:0.4,
+    envMap: environmentMapTexture
+})
+
 
 
 const createSphere = (radius, position) => {
     // Three.js mesh creación de la maya que creamos comunmente 
-    const mesh = new THREE.Mesh(
-        new THREE.SphereBufferGeometry(radius,20,20),
-        new THREE.MeshStandardMaterial({
-            metalness:0.3,
-            roughness:0.4,
-            envMap: environmentMapTexture
-        })
-    )
+    const mesh = new THREE.Mesh(sphereGeometry,sphereMaterial); 
+    mesh.scale.set(radius, radius, radius); 
     mesh.castShadow = true;
     mesh.position.copy(position); // mesh es un objeto donde accedemos a la propiedad position y le le decimos que copie en esa propiedad la nueva position dada por la función 
     scene.add(mesh); 
@@ -253,11 +269,50 @@ const createSphere = (radius, position) => {
     })
 }
 
+const boxGeometry = new THREE.BoxBufferGeometry(1 ,1 ,1);
+const boxMaterial = new THREE.MeshStandardMaterial({
+    metalness: 0.5,
+    roughness: 0.5,
+    envMap: environmentMapTexture
+})
+
+
+const createBox = (width, height, depth, position) =>{
+    // Three.js creacion del mesh
+    const mesh = new THREE.Mesh(boxGeometry, boxMaterial);
+    mesh.scale.set(width, height, depth);
+    mesh.castShadow = true;
+    mesh.position.copy(position);
+    scene.add(mesh);
+
+    // Cannon.js
+    const shape = new CANNON.Box(new CANNON.Vec3(width / 2, height / 2, depth /2));
+    const body = new CANNON.Body({
+        mass:1,
+        position: new CANNON.Vec3(0,3,0),
+        shape: shape,
+        material: defaultMaterial
+    })
+    body.position.copy(position);
+    // console.log(body)
+    world.addBody(body);
+
+    objectsToUpdate.push({
+        mesh:mesh,
+        body:body
+    })
+
+}
+
 // Llamamos la función de crear las esferas "createSphere"
 createSphere(0.5,{x:0 , y:3, z:0});
+//createBox(0.5,{x:0 , y:3, z:0}); 
 // createSphere(0.5,{x:2 , y:3, z:2});
 // createSphere(0.5,{x:1 , y:1, z:4});
 // console.log(objectsToUpdate);
+
+
+
 /**
  * Animate
  */
